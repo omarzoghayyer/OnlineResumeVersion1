45bc443a35a7ca5d04ae6e8c37e2986c11feddd5
Omar Zoghayyer <sfzoghayyer@gmail.com>
2022-12-25 11:38:18 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const Resume = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="resume">
+      <h1>Omar Zoghayyer</h1>
+      <p>Contact Information:</p>
+      <ul>
+        <li>Email: sfzoghayyer@gmail.com</li>
+        <li>Phone: 415 980 9396</li>
+      </ul>
+      <p>Summary:</p>
+      <p>
+       Software Engineer in mind.
+      </p>
+      <p>Experience:</p>
+      <ul>
+        <li>
+          ELectronic Arts (2020-present)
+          <ul>
+            <li>Position: Quality Disgner I and  Software Engineer</li>
+            <li>
+              Responsibilities:
+              <ul>
+                <li>Developed new features for the company's product</li>
+                <li>Fixed bugs and issues reported by customers</li>
+                <li>Participated in code reviews and team meetings</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          Company B (2018-2020)
+          <ul>
+            <li>Position: Junior Software Engineer</li>
+            <li>
+              Responsibilities:
+              <ul>
+                <li>Assisted in the development of new features</li>
+                <li>Helped debug and fix issues</li>
+                <li>Learned new technologies and programming languages</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <p>Education:</p>
+      <ul>
+        <li>
+          University of XYZ (2014-2018)
+          <ul>
+            <li>Degree: Bachelor's in Computer Science</li>
+            <li>GPA: 3.8/4.0</li>
+          </ul>
+        </li>
+      </ul>
+      <p>Skills:</p>
+      <ul>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Node.js</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+    <dev>
+      Written by ChatGBT
+    </dev>
     </div>
   );
-}
+};
 
-export default App;
+export default Resume;
